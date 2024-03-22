334f46426358aa375787fd47d3ce384d9d480f59
arnnvv <arnav.900@protonmail.com>
2024-03-22 06:09:25 +0530

    setup

--- index.ts
+++ index.ts
@@ -1,1 +1,9 @@
-console.log("Hello via Bun!");
+import expess from "express";
+
+const app = expess();
+
+app.use(expess.json());
+
+app.listen(process.env.PORT || 3000, () => {
+  console.log(`Server is running on port ${process.env.PORT || 3000}`);
+});
